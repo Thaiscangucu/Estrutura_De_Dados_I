e83725d777bba4799f28fdaca03efe2fab9aab35
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2024-11-02 14:36:31 -0300

    Update Par.java

--- Projeto2/Par.java
+++ Projeto2/Par.java
@@ -1,1 +1,35 @@
+public class Par {
+    private char registrador;
+    private int valor;
 
+    public Par(char registrador, int valor) {
+        this.registrador = registrador;
+        this.valor = valor;
+    }
+
+    public Par() {
+        this.registrador = ' ';
+        this.valor = 0;
+    }
+
+    public char getRegistrador() {
+        return registrador;
+    }
+
+    public void setRegistrador(char registrador) {
+        this.registrador = registrador;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Par(" + registrador + " " + valor + ')';
+    }
+}
