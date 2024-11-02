cc990a99a603467427be008fec4faec9295204f9
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2024-11-02 14:36:17 -0300

    Update Node.java

--- Projeto2/Node.java
+++ Projeto2/Node.java
@@ -1,1 +1,37 @@
+public class Node {
+  private String dado;
+  private Node prox;
 
+  public Node() {
+    this(null, null);
+  }
+
+  public Node(String dado, Node prox) {
+    this.dado = dado;
+    this.prox = prox;
+  }
+
+  public Node getProx() { 
+    return prox; 
+  }
+
+  public String getDado() { 
+    return dado; 
+  }
+
+  public void setProx(Node prox) { 
+    this.prox = prox; 
+  }
+
+  public void setDado(String dado) { 
+    this.dado = dado; 
+  }
+
+  @Override
+  public String toString() {
+      return "Node{" +
+             "dado=" + dado +
+             ", prox=" + (prox != null ? prox.getDado() : "null") +
+             '}';
+  }
+}
