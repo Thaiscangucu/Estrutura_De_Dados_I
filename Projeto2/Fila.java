7d2a93b41f5afe96ecce02ab6da4dca81eb2e55d
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2024-11-05 00:49:29 -0300

    Update Fila.java

--- Projeto2/Fila.java
+++ Projeto2/Fila.java
@@ -1,101 +1,113 @@
 public class Fila<T> {
 
-    private static final int TAMANHO_PADRAO = 26;
-    private T dados[];
-    private int contagem;
-    private int primeiro;
-    private int ultimo;
-    private Par parAtual;
+  private static final int TAMANHO_PADRAO = 26;
+  private T dados[];
+  private int contagem;
+  private int primeiro;
+  private int ultimo;
+  private Par parAtual;
 
-    public Fila() {
-        this(TAMANHO_PADRAO);
+  public Fila() {
+    this(TAMANHO_PADRAO);
+  }
+
+  @SuppressWarnings("unchecked")
+  public Fila(int tamanho) {
+    dados = (T[]) new Object[tamanho];
+    contagem = 0;
+    primeiro = 0;
+    ultimo = 0;
+  }
+
+  public void enqueue(T valor) {
+    if (isFull()) {
+      throw new RuntimeException("enqueue(): fila cheia.");
     }
+    dados[ultimo] = valor;
+    contagem++;
+    ultimo = (ultimo + 1) % dados.length;
+  }
 
-    @SuppressWarnings("unchecked")
-    public Fila(int tamanho) {
-        dados = (T[]) new Object[tamanho];
-        contagem = 0;
-        primeiro = 0;
-        ultimo = 0;
+  public boolean isFull() {
+    return contagem == dados.length;
+  }
+
+  public T dequeue() {
+    if (isEmpty()) {
+      throw new RuntimeException("dequeue(): a fila está vazia.");
     }
+    T valor = dados[primeiro];
+    primeiro = (primeiro + 1) % dados.length;
+    contagem--;
+    return valor;
+  }
 
-    public void enqueue(T valor) {
-        if (isFull()) {
-            throw new RuntimeException("enqueue(): fila cheia.");
-        }
-        dados[ultimo] = valor;
-        contagem++;
-        ultimo = (ultimo + 1) % dados.length;
+  public Par search(Fila<Par> registradores, char registrador) {
+      Par resultado = null;
+      int tamanhoFila = registradores.size(); 
+      for (int i = 0; i < tamanhoFila; i++) {
+          Par parAtual = registradores.dequeue();
+          if (parAtual.getRegistrador() == registrador && resultado == null) {
+              resultado = parAtual; 
+          }
+          registradores.enqueue(parAtual);
+      }
+      return resultado; 
+  }
+  
+  public Par searchDequeue(Fila<Par> registradores, char registrador) {
+    for (int i = 0; i < 26; i++) {
+      parAtual = registradores.dequeue();
+      if (parAtual.getRegistrador() == registrador) {
+        return parAtual;
+      } else {
+        registradores.enqueue(parAtual);
+      }
     }
+    return null;
+  }
 
-    public boolean isFull() {
-        return contagem == dados.length;
+  public boolean isEmpty() {
+    return contagem == 0;
+  }
+
+  public T front() {
+    if (isEmpty()) {
+      throw new RuntimeException("front(): fila vazia");
     }
+    return dados[primeiro];
+  }
 
-    public T dequeue() {
-        if (isEmpty()) {
-            throw new RuntimeException("dequeue(): a fila está vazia.");
-        }
-        T valor = dados[primeiro];
-        primeiro = (primeiro + 1) % dados.length;
-        contagem--;
-        return valor;
+  public T back() {
+    if (isEmpty()) {
+      throw new RuntimeException("back(): fila vazia");
     }
+    int indice = (dados.length + ultimo - 1) % dados.length;
+    return dados[indice];
+  }
 
-    public Par searchDequeue(Fila<Par> registradores, char registrador) {
-        for(int i = 0; i < 26; i++){
-            Par parAtual = registradores.dequeue();
-            if(parAtual.getRegistrador() == registrador ){
-                return parAtual;
-            }
-            else{
-                registradores.enqueue(parAtual);
-            }
-        }
-        return null;
+  public int size() {
+    return dados.length;
+  }
+
+  public int getCount() {
+    return contagem;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Fila: [");
+
+    for (int i = 0; i < contagem; i++) {
+      int index = (primeiro + i) % dados.length;
+      sb.append(dados[index]);
+
+      if (i < contagem - 1) {
+        sb.append(", ");
+      }
     }
-
-    public boolean isEmpty() {
-        return contagem == 0;
-    }
-
-    public T front() {
-        if (isEmpty()) {
-            throw new RuntimeException("front(): fila vazia");
-        }
-        return dados[primeiro];
-    }
-
-    public T back() {
-        if (isEmpty()) {
-            throw new RuntimeException("back(): fila vazia");
-        }
-        int indice = (dados.length + ultimo - 1) % dados.length;
-        return dados[indice];
-    }
-
-    public int size() {
-        return dados.length;
-    }
-
-    public int getCount() {
-        return contagem;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Fila: [");
-
-        for (int i = 0; i < contagem; i++) {
-            int index = (primeiro + i) % dados.length;
-            sb.append(dados[index]);
-
-            if (i < contagem - 1) {
-                sb.append(", ");
-            }
-        }
-        sb.append("]");
-        return sb.toString();
-    }
+    sb.append("]");
+    return sb.toString();
+  }
 }
